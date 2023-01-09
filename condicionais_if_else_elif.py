ac96d31b064477133ee297df7695f6cffa20c0c8
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-09 20:33:21 -0300

    atualização

--- condicionais_if_else_elif.py
+++ condicionais_if_else_elif.py
@@ -1,12 +1,12 @@
 #print ('ola mundo!!!')
 
-""""
- Estruturas condicionais
- if, else, elif
-"""
 
- idade = 6
- if idade < 18:
+ #Estruturas condicionais
+ #if, else, elif
+
+
+idade = 26
+if idade < 18:
     print('Menor de idade')
 else:
     print('Maior de idade')
