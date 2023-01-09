d122252c8fc76fe182424d02d219725bda7991a3
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-09 20:59:38 -0300

    atualização

--- condicionais_if_else_elif.py
+++ condicionais_if_else_elif.py
@@ -18,14 +18,14 @@ else:
 if idade < 18:
     print('menor de idade')
 
-if idade  18:
-    print('menor de idade')
+if idade == 18:
+    print('tem 18 anos')
 
-if idade < 18:
-    print('menor de idade')
+if idade == 26:
+    print('tem 26 anos')
 
-if idade < 18:
-    print('menor de idade')
+if idade > 18:
+    print('maior de idade')
 
 #em python um novo bloco é iniciado quando se coloca os dois pontos :
 # sempre lenbrando da regra de identação 4 espaços
