59faef2adb800e4081049e2e3361f909fe51e809
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-10 21:11:25 -0300

    atualização

--- loop_for.py
+++ loop_for.py
@@ -6,4 +6,13 @@ for -> uma dessas estruturas
 
 for item in interavel:
     //execução do loop
+
+ultilizamos loops para interar sobre sequencia ou sobre valores iteraveis
+
+exemplos  de interáveis:
+string
+nome = 'geek university'
+lista
+
+
 """
