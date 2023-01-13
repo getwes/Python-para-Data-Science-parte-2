4689c2ae8aaacfb89bfb71e1b48e3853fb991810
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-12 22:30:43 -0300

    atualização

--- entendendo_ranges.py
+++ entendendo_ranges.py
@@ -23,11 +23,21 @@ for num in range(11):
 #obs: valor_de_paradaa não inclui (inicio especificodo pelo usuario, e passo de 1 em 1)
 for num in range(1, 11):
     print(num)
-"""
+
 #exemplo forma 3
 
 #range(valor_de_inicio, valor_de_parada)
 #obs: valor_de_paradaa não inclui (inicio especificodo pelo usuario, e passo especificado pelo usuario )
 
 for num in range(1, 10, 2): # começã com o numero 1 vai ate o numero 10 e vai pulando de 2 em 2 numeros
+    print(num)
+"""
+#forma 4 (inversa)
+
+#range(valor_de_inicio, valor_de_parada, passo)
+
+#obs: valor_de_paradaa não inclui (inicio especificodo pelo usuario, e passo especificado pelo usuario )
+
+# exemplo forma 4
+for num in range(10, 0, -1 ):
     print(num)
