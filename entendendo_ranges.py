b3e7d1a6f4b9234700571ec91585021c7f86066b
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-12 22:04:28 -0300

    atualização

--- entendendo_ranges.py
+++ entendendo_ranges.py
@@ -2,4 +2,14 @@
 Ranges
 
 - precisamos conhecer o loop for para usar os ranges.
+precisamos conhcer o range para trabalhar melhor com o loops for
+
+ranges são utilizadas para gerar sequencias numerica, 
+não de forma aleatroira, mas sim de maneira especificada.
+
+forma gerais:
+
+range(valor_de_parada)
+
+obs:
 """
