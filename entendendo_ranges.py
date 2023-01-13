e5828d54ba0c8c6b01935ee4ef9a79e7f95c61c0
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-12 22:14:44 -0300

    atualização

--- entendendo_ranges.py
+++ entendendo_ranges.py
@@ -13,8 +13,13 @@ range(valor_de_parada)
 
 obs: valor_de_parada não  inclusive (inicio padrão 0, e passo de 1 em 1)
 
-"""
-#forma 1
+#exemplo forma 1
 
 for num in range(11):
+    print(num)
+"""
+#forma 2
+#range(valor_de_inicio, valor_de_parada)
+#obs: valor_de_paradaa não inclui (inicio especificodo pelo usuario, e passo de 1 em 1)
+for num in range(1, 11):
     print(num)
