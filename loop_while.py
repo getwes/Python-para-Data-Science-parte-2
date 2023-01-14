aee1511317d4d21c28bcd1afbe5508ec2035b173
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-13 23:33:12 -0300

    atualização

--- loop_while.py
+++ loop_while.py
@@ -11,4 +11,6 @@ o bloco do while sera repetido enquanto a expressão booleana for verdadeira.
 expressão booleada é toda expressão onde o resultado é verdadeiro ou falso.
 
 exemplo:
+
+num = 5
 """
