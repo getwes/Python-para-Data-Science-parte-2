94bf640359e7f4e8f22ea2f741611b4272c3f1d0
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-13 23:32:22 -0300

    atualização

--- loop_while.py
+++ loop_while.py
@@ -7,4 +7,6 @@ while expressão_booleana:
     #execuçaõ do loop
 
 o bloco do while sera repetido enquanto a expressão booleana for verdadeira.
+
+expressão booleada é toda expressão onde o resultado é verdadeiro ou falso.
 """
