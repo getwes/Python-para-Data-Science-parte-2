553165963bcb8121f543d249b66beb0d75f6fc8c
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-14 23:08:43 -0300

    atualização

--- LISTAS.py
+++ LISTAS.py
@@ -5,6 +5,9 @@
   de serem DINAMICO  e tambem de podermos colocar QUALQER tipo de dado.
 
   linguagens c/java: arrays
+  possuem tamanho e tipo de dados fixo;
+  ou seja,  nestas linguagens se voce criar um array do tipo int e com tamanho 5, este array
+  sera sempre do tipo interio e podera ter SEMPRE no maximo 5 valores.
 
   - dinamico: 
 """
