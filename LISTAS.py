7d232be45bdbdeb498e71329bcacbfab96a65234
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-14 23:06:12 -0300

    atualização

--- LISTAS.py
+++ LISTAS.py
@@ -3,4 +3,8 @@
 
   listas em python funcional como vetores/matrizes (arrys) em outras linguagnes, com a diferença
   de serem DINAMICO  e tambem de podermos colocar QUALQER tipo de dado.
+
+  linguagens c/java: arrays
+
+  - dinamico: 
 """
