f5ad6d82c72a9b07845a200787899eaacf0fad21
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-14 21:59:19 -0300

    atualização

--- breaks.py
+++ breaks.py
@@ -5,3 +5,11 @@ funciona da mesma forma que nas linguagens c ou java.
 
 ultilizamos o break para sair de loops de maneira projetada.
 """
+
+# exemplo 1
+
+for numero in range(1, 11):
+    if numero == 6:
+        break
+    else:
+        print(numero)
