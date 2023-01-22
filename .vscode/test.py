0358a0d30f9743cfc8d7dd841763e2143152ffc3
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-22 12:15:20 -0300

    atualização

--- .vscode/test.py
+++ .vscode/test.py
@@ -4,3 +4,15 @@ while numero < 10:
     print(numero)
     numero = numero + 1
 """
+
+type([])
+
+lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
+
+lista2 = ['w', 'e', 's', 'l', 'e', 'y']
+
+lista3 = []
+
+lista4 = list(range(11))
+
+lista5 = list('Wesley da Silva Freire')
