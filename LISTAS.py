3169f54eee15f768369a5633681d13175343fa47
Wesley Freire <wesleyfreire1707@gmail.com>
2023-01-25 22:41:42 -0300

    atualização

--- LISTAS.py
+++ LISTAS.py
@@ -117,6 +117,7 @@ lista5 = list('Wesley da Silva Freire')
 
 # podemos remover um elemento pelo indice
 #obs os elementos a direita deste indicee serão deslocados para a esquerda.
+#obs se não houver elemento no indice informado, rteremos o erro indexerror.
 print(lista5)
 lista5.pop(2)
 print(lista5)
