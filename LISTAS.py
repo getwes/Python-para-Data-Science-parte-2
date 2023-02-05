6ef14a4a81b7d60314826a141112339682c5e3ca
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-05 18:34:08 -0300

    atualização

--- LISTAS.py
+++ LISTAS.py
@@ -219,7 +219,7 @@ print(cores[3]) # branco
 
 #fazer acesso aos elementos de forma indexado inversa
 
-print(cores[0]) 
-print(cores[1]) 
-print(cores[2]) 
-print(cores[3]) 
+print(cores[-1]) 
+print(cores[-2]) 
+print(cores[-3]) 
+print(cores[-4s]) 
