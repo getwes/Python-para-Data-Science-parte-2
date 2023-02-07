7ccec989841c37a8ec85bcfb43e1039d6cecc0c8
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-06 21:19:16 -0300

    atualização

--- tuplas.py
+++ tuplas.py
@@ -6,5 +6,8 @@ tuplas (tuple)
  existem basicamente duas diferenças basicas:
 
  1 - as tuplas são representadas por parenteses ()
+
+ 2 - as tuplas são imutaveis: isso significa que ao se criar uma tupla ela não muda. toda
+ operação em uma tupla gera uma nova tupla.
 """
 print(type(()))
