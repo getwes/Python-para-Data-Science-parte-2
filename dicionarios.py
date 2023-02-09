0c324858b360288ec0d8a440c595e7463ea055db
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-09 19:07:07 -0300

    atualização

--- dicionarios.py
+++ dicionarios.py
@@ -26,3 +26,8 @@ paises = dict(br = 'brasil', eua = 'estados unidos', py = 'paraguai')
 print (paises)
 print(type(paises))
 """
+paises = {'br': 'brasil', 'eua': 'estados unidos', 'py': 'paraguai'}
+
+#acessando elementos
+
+#froma 1 - acessando via chave, da mesma froma que lista/tupla
