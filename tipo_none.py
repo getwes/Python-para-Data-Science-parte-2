68f2b4496414295626683221ebab639847bf14f9
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-09 20:31:45 -0300

    atualização

--- tipo_none.py
+++ tipo_none.py
@@ -1,3 +1,4 @@
 """
-otipo de dado none
+otipo de dado none em python representa o tipo sem tipo, ou poderia ser conhecido tambem como 
+tipo vazio, falar que é um tipo sem tipo é mais apropriado.
 """
