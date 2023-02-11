66cbcc24d063e5271e638b9650d536b85f47940e
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-10 21:26:01 -0300

    atualização

--- tipo_none.py
+++ tipo_none.py
@@ -3,6 +3,11 @@ otipo de dado none em python representa o tipo sem tipo, ou poderia ser conhecid
 tipo vazio, falar que é um tipo sem tipo é mais apropriado.
 
 obs: o tipo none é sempre especificado com a primenra letra maiuscula.
+
+certo: None
+errado: none
+
+quando ultilizamos??
 """
 
 numero = None
