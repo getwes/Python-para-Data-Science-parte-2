f906ff8097fbb9a8d017604d014be3eb221cd399
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-20 20:35:10 -0300

    atualização

--- mapas.py
+++ mapas.py
@@ -4,7 +4,10 @@ mapas -> conhechecidos como dicionarios
 dicionarios em python são representados por chavess {}
 """
 receita = {'jan': 100, 'fev': 120, 'mar': 300}
-
+print(receita)
 #interar sobre dicionarios
 for chave in receita:
     print(chave)
+
+for chave in receita:
+    print(receita[chave])
