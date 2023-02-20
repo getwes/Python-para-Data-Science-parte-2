0fcc7d65e02d3cb9e5c6aa6cee89a7018ffb010b
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-20 20:38:00 -0300

    atualização

--- mapas.py
+++ mapas.py
@@ -7,7 +7,7 @@ receita = {'jan': 100, 'fev': 120, 'mar': 300}
 print(receita)
 #interar sobre dicionarios
 for chave in receita:
-    print(chave)
+    print(chave) # aqui estamos imprimindo apenas as chaves
 
 for chave in receita:
-    print(receita[chave])
+    print(receita[chave])  # nesse estamos imprimendo  apenas os valores 100 , 120 , 300
