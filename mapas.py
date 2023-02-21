6568d63259e9bbdfbbb1b7605590fe3b808434a8
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-20 21:58:05 -0300

    atualização

--- mapas.py
+++ mapas.py
@@ -28,3 +28,7 @@ for valor in receita.values():
 receita = {'jan': 100, 'fev': 120, 'mar': 300}
 print(receita)
 
+#desempacotamento de dicionarios
+print(receita.items())
+for chave, valor in receita.items():
+    print(f'chave={chave} e valor={valor}')
