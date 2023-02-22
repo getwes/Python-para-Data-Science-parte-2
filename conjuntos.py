20a716ec666f03b68d6d24c239bf9e28a5c15451
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-21 21:08:02 -0300

    atualização

--- conjuntos.py
+++ conjuntos.py
@@ -1,4 +1,6 @@
 """
 - conjuntos em qual quer linguagem de programação , estamos fazendo referencia a teoria dos conjuntos
  da matematica.
-"""
+
+- aqui no python, os conjuntos são chamados de sets.
+"""s
