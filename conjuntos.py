bb51abd8f43ea6d967e9d54027c1e0b3fb7ab6da
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-22 23:07:28 -0300

    atualização

--- conjuntos.py
+++ conjuntos.py
@@ -51,13 +51,15 @@ lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27, 27, 99, 13, 12, 13, 2, 2]
 print(set(lista1))
 """
 # importante lembrar que, além de não termos valores duplicado não temos ordem
-lista = [99, 2, 34, 23, 12, 1,44, 5]
+
+dados = '99, 2, 34, 23, 2, 12, 1,44, 5, 34'
+lista = list(dados)
 print(f'lista: {lista}')
 
-tupla = (99, 2, 34, 23, 12, 1,44, 5)
+tupla = tuple(dados)
 print(f'tupla: {tupla}')
 
-dicionario = (lista, 'dict')
+dicionario = (dados, 'dict')
 print(f'dicionario: {dicionario}')
-conjunto = {99, 2, 34, 23, 12, 1,44, 5}
+conjunto = set(dados)
 print(f'conjunto: {conjunto}')
