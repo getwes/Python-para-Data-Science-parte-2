0e0bbb1bfb50f04f5f0804116b24dbc15f3a87c5
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-25 19:10:05 -0300

    atualização]

--- counter.py
+++ counter.py
@@ -9,9 +9,9 @@ de ocorrencias desse elemento.
 
 """
 # ultilizando o counter
-
+#realizando
 from collections import Counter
 # podemos ultilizar qualquer interavel, aqui usando uma lista
 lista = [1, 1, 1, 2, 2, 3, 3, 3, 3, 1, 1, 2, 2, 4, 4, 4, 5, 5, 5, 5, 5, 5, 3, 45, 45, 66, 66, 43, 34, 100]
-
+# ultilizando o counter
 res = Counter(lista)
