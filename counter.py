7898143735adb011c71f999a431b746ee1c79c5e
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-25 18:59:09 -0300

    atualização

--- counter.py
+++ counter.py
@@ -6,4 +6,8 @@ collections - high-performance container datetypes
 counter - recebe um interavel como parametro e cria um objeto do tipo collections counter que é parecido
 com um dicionario,contendo como chave o elemento da lista passada como parametro e como valor a quantidade
 de ocorrencias desse elemento.
+
 """
+# ultilizando o counter
+
+from collections import Counter
