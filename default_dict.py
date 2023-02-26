281828300a5c5d50b17c3b2c2f2556e636237519
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-26 18:26:23 -0300

    atualização

--- default_dict.py
+++ default_dict.py
@@ -11,5 +11,9 @@ print(dicionario['curso'])
 
 print(dicionario['outro']) # ??? key error
 
+default dict -> ao criar um dicionario ultilizando-o, nos informamos um valor default,
+podendo utilizar um lambda para isso. este valor sera utilizado sempre que não houver
+um valor definido. caso tentemos acessar uma chve que não existe, essa chave sera 
+criada e o valor default sera attribuido.
 """
 
