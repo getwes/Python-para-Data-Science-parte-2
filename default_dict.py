2abfb03a87f9f9c09c4cdfff9dd717c4727081ca
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-26 18:19:14 -0300

    atualização

--- default_dict.py
+++ default_dict.py
@@ -4,4 +4,8 @@ modulo collections - default dict
 
 dicionario = {'curso': 'programação em python: essencial'}
 
-print('curso')
+print(dicionario)
+
+print(dicionario['curso'])
+
+print(dicionario['outro']) # ??? key error
