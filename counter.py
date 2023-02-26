87e823833007f2729b54610c8e84b579eb20dcf5
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-26 16:50:48 -0300

    atualização

--- counter.py
+++ counter.py
@@ -7,10 +7,11 @@ counter - recebe um interavel como parametro e cria um objeto do tipo collection
 com um dicionario,contendo como chave o elemento da lista passada como parametro e como valor a quantidade
 de ocorrencias desse elemento.
 
-"""
 # ultilizando o counter
 #realizando
 from collections import Counter
+
+#exemplo 1
 # podemos ultilizar qualquer interavel, aqui usando uma lista
 lista = [1, 1, 1, 2, 2, 3, 3, 3, 3, 1, 1, 2, 2, 4, 4, 4, 5, 5, 5, 5, 5, 5, 3, 45, 45, 66, 66, 43, 34, 100]
 # ultilizando o counter
@@ -22,3 +23,5 @@ print(res)
 #Counter({5: 6, 1: 5, 3: 5, 2: 4, 4: 3, 45: 2, 66: 2, 43: 1, 34: 1, 100: 1})
 
 #veja que, para cada elemento da lista, o counter criou uma chave e colocou como valor a quantidade de ocorrencias.
+
+"""
