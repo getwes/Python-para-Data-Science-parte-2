e92d63fa1af1853d312cd785e097cf16900fb1f8
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-27 22:50:11 -0300

    atualização

--- ordered_dict.py
+++ ordered_dict.py
@@ -33,4 +33,11 @@ dict2 = {'b': 2, 'a': 1}
 
 print( dict1 == dict2) #True -> ja que a ordem dos elementos não importa para o dicionario
 
-#
+#ordered dict
+
+from collections import OrderedDict
+
+odict1 = OrderedDict({'a': 1, 'b': 2})
+odict2 = OrderedDict({'b': 2, 'a': 1})
+
+print( odict1 == odict2) # false -> ja que a ordem dos elementos importa para o ordereddict
