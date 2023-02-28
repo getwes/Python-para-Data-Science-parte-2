b557425bf090797e186dc0f25d06e0b688375cbf
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-27 22:33:01 -0300

    atualização

--- ordered_dict.py
+++ ordered_dict.py
@@ -14,3 +14,7 @@ for chave, valor in dicionario.items():
 #print(dicionario.items())
 #python library
 """
+# fazendo import
+from collections import OrderedDict
+
+dicionario =OrderedDict({'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5})
