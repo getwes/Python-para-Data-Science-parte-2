2c56608b3e547f0ddf5d8695d6679837d026053b
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-27 22:36:07 -0300

    atualização

--- ordered_dict.py
+++ ordered_dict.py
@@ -15,7 +15,7 @@ for chave, valor in dicionario.items():
 #python library
 
 ordereddict -> é um dicionario, que nos garante a ordem de inserção dos elementos.
-"""
+
 # fazendo import
 from collections import OrderedDict
 
@@ -23,3 +23,4 @@ dicionario =OrderedDict({'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5})
 
 for chave, valor in dicionario.items():
     print(f'chave={chave}:valor={valor}')
+"""
