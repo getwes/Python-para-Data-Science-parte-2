43ff8b51ea4f7d6017dc7930eb0dd9fc8efdad13
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-27 21:26:46 -0300

    atualização

--- ordered_dict.py
+++ ordered_dict.py
@@ -1,7 +1,7 @@
 """
   modulo collection: ordered dict
 """
-
+# em dicionario, a ordem de inserção dos elementos não é garatinda.
 dicionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
 
 print(dicionario)
