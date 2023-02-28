30f2f2f66b618e23abb4c859b046a4f1707bc315
Wesley Freire <wesleyfreire1707@gmail.com>
2023-02-27 22:39:35 -0300

    atualização

--- ordered_dict.py
+++ ordered_dict.py
@@ -24,3 +24,6 @@ dicionario =OrderedDict({'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5})
 for chave, valor in dicionario.items():
     print(f'chave={chave}:valor={valor}')
 """
+# entendendo a diferença entre dict e ordered dict
+
+# dicionanrios comuns
