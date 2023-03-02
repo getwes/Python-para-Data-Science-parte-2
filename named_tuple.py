4b17a693d8c9b7f749c7dbade4944e94d168a2c5
Wesley Freire <wesleyfreire1707@gmail.com>
2023-03-01 22:27:56 -0300

    atualização

--- named_tuple.py
+++ named_tuple.py
@@ -5,6 +5,8 @@ modulo collection - named tuple
 tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 print(tupla[1])
+
+named tuple -> são tuplas, difereciada, onde, especificamos um nome para a mesma e tambem parametros.
 """
 
 
