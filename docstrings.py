bbcdb5eff19c006ee3e59f4c79af2259ceba7c16
Wesley Freire <wesleyfreire1707@gmail.com>
2023-03-14 21:57:37 -0300

    atualização

--- docstrings.py
+++ docstrings.py
@@ -1,3 +1,20 @@
 """
 documentando funçõs com docstrings
+
+obs: podemos ter acesso a documentação de uma função em python
+ultilizando a propriedade especial__doc__
+
+podemos ainda fazer acesso a documentação com a função help()
 """
+def diz_oi():
+    """ uma função simples que retorna a string oi"""
+    return 'oi'
+
+print(diz_oi())
+
+def exponencial(numero, potencia=2):
+    """
+    podemos fazer documentação dentro  de funções
+    """
+    return numero ** potencia
+
